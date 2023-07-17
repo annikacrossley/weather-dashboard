63867f80d4fa7f0cdf4b4118e3e90370d73181e3
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-07-17 18:04:55 -0500

    got search history and buttons working - thanks instructor josh!

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@
 
 let APIkey = "5ae0c255890061ed7b0bad5e03c377e9"
 let fetchButton = document.getElementById('search-button')
+let historyEl = document.getElementById('history')
 
 function getApi(cityName) {
     let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + APIkey + '&units=imperial'
@@ -32,59 +33,63 @@ function forecast (latitude, longitude) {
         return response.json()
     }).then(function(data) {
         console.log(data)
+        //reset the innerHTML of the container (empty it)
+        container.innerHTML = ""
         for (let i = 0; i < data.list.length; i++) {
-            let card = document.createElement("div")
-            let date = document.createElement("p")
-            let temp = document.createElement("li")
-            let wind = document.createElement("li")
-            let humidity = document.createElement("li")
             if (data.list[i].dt_txt.includes("12:00:00")) {
+                let card = document.createElement("div")
+                let date = document.createElement("p")
+                let temp = document.createElement("li")
+                let wind = document.createElement("li")
+                let humidity = document.createElement("li")
                 console.log(data.list[i])
                 date.textContent = data.list[i].dt_txt.split(' ')[0]
                 temp.textContent = "Temperature: " + data.list[i].main.temp + "F"
                 wind.textContent = "Wind: " + data.list[i].wind.speed + "mph"
                 humidity.textContent = "Humidity: " + data.list[i].main.humidity + "%"
+                container.appendChild(card)
+                card.appendChild(date)
+                card.appendChild(temp)
+                card.appendChild(wind)
+                card.appendChild(humidity)
             }
-           container.appendChild(card)
-           card.appendChild(date)
-           card.appendChild(temp)
-           card.appendChild(wind)
-           card.appendChild(humidity)
         }
     })
 }
 
+function createHistoryButton(citySearch){
+    let button = document.createElement('button')
+    button.textContent = citySearch
+    button.addEventListener('click', function(event){
+        let cityName = event.target.innerText
+        getApi(cityName)
+    })
+    historyEl.appendChild(button)
+}
+
+function getStoredCities () {
+    let storedCities = JSON.parse(localStorage.getItem("history")) || []
+    return storedCities
+}
+
+function loadSearchHistory() {
+    let storedCities = getStoredCities()
+    for (let i=0; i<storedCities.length; i++) {
+        createHistoryButton(storedCities[i])
+    }
+}
+
 function addSearchHistory(cityName) {
+    let storedCities = getStoredCities()
     storedCities.push(cityName)
     localStorage.setItem("history", JSON.stringify(storedCities))
 }
 
-// function displayHistory() {
-//     let cityList = document.getElementById("city-list")
-//     cityList.innerHTML = " "
-//     let storedCities = getLocalStorage('storedCities')
-//     if (storedCities) {
-//         storedCities.reverse()
-//         for(i=0; i<storedCities.length; i++) {
-//             let city = storedCities[i]
-//             let item = document.createElement('button')
-//             item.textContent = city
-//             item.addEventListener('click', function() {
-//                 document.querySelector("#city-search").value = this.textContent
-//                  citySearch(this.textContent)
-//             })
-//             cityList.appendChild(item)
-//         }
-//     }
-// }
-
 fetchButton.addEventListener('click', function() {
     let citySearch = document.querySelector("#city-search").value
     getApi(citySearch)
-    let storedCities = JSON.parse(localStorage.getItem("history")) || []
-    storedCities.push({
-        city:citySearch
-    })
-    localStorage.setItem("history", JSON.stringify(storedCities))
-    })
-;
+    createHistoryButton(citySearch)
+    addSearchHistory(citySearch)
+ });
+
+loadSearchHistory()
